10642c6259760f79416da074f979c0dc57ca498e
Harut <mr.harutyun@bk.ru>
2018-02-17 12:34:29 +0400

    few andjst.

--- yii2-app-basic/views/layouts/main.php
+++ yii2-app-basic/views/layouts/main.php
@@ -69,9 +69,9 @@ AppAsset::register($this);
 
 <footer class="footer">
     <div class="container">
-        <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
+        <p class="pull-left">&copy; Created by Harutyun Gazyan for DEVELANDOO </p>
 
-        <p class="pull-right"><?= Yii::powered() ?></p>
+
     </div>
 </footer>
 
